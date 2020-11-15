55083f1a8e9741d62c0625a93a65f6a1dd3b7512
0918nobita <nobita.0918@gmail.com>
2020-11-15 15:45:46 +0900

    :sparkles: html 文書の内容をバイナリに埋め込むようにした

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
@@ -6,6 +5,10 @@ use std::time::Duration;
 
 extern crate server;
 use server::thread_pool::ThreadPool;
+
+const INDEX_HTML: &'static [u8] = include_bytes!("../index.html");
+
+const NOT_FOUND_HTML: &'static [u8] = include_bytes!("../404.html");
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -29,16 +32,15 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, filename) = if buf.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    let (status_line, contents) = if buf.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", String::from_utf8_lossy(INDEX_HTML))
     } else if buf.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", String::from_utf8_lossy(INDEX_HTML))
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", String::from_utf8_lossy(NOT_FOUND_HTML))
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
     let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
